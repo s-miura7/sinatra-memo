f9b2bf7d4f5007fc86bc4ae52cbda9749f409cd6
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-05-17 10:37:21 +0900

    完成

---
+++ main.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'debug'
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+get '/memos' do
+  @memos = File.open('data.json') { |file| JSON.parse(file.read) }
+  erb :top_view
+end
+
+get '/memos/new' do
+  erb :new_view
+end
+
+post '/memos/new' do
+  current_datas = File.open('data.json') { |file| JSON.parse(file.read) }
+  File.open('data.json', 'w') do |file|
+    req_body = {}
+    req_body['title'] = h(params[:title])
+    req_body['text'] = h(params[:text])
+    req_body['id'] = current_datas.empty? ? 1 : current_datas[-1]['id'] + 1
+    current_datas.push(req_body)
+    JSON.dump(current_datas, file)
+  end
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+  id = params['id'].to_i
+  current_datas = File.open('data.json') { |file| JSON.parse(file.read) }
+  @memo = current_datas.filter { |data| data['id'].eql?(id) }[0]
+  erb :show_view
+end
+
+delete '/memos/:id' do
+  id = params['id'].to_i
+  current_datas = File.open('data.json') { |file| JSON.parse(file.read) }
+  current_datas.delete_if { |data| data['id'].eql?(id) }
+  File.open('data.json', 'w') { |file| JSON.dump(current_datas, file) }
+  redirect '/memos'
+end
+
+get '/memos/:id/edit' do
+  p params
+  id = params['id'].to_i
+  current_datas = File.open('data.json') { |file| JSON.parse(file.read) }
+  @memo = current_datas.filter { |data| data['id'].eql?(id) }[0]
+  erb :edit_view
+end
+
+patch '/memos/:id/edit' do
+  p params
+  id = params['id'].to_i
+  current_datas = File.open('data.json') { |file| JSON.parse(file.read) }
+  File.open('data.json', 'w') do |file|
+    req_body = {}
+    req_body['title'] = h(params[:title])
+    req_body['text'] = h(params[:text])
+    req_body['id'] = id
+    current_datas.map! { |data| data['id'] == id ? req_body : data }
+    JSON.dump(current_datas, file)
+  end
+  redirect '/memos'
+end
